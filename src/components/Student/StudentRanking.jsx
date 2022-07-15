cb72115fee1ef7e6c8c6f5cf6df0b285e4fa6113
TineoC <christophertineo02@gmail.com>
2022-07-15 02:41:12 -0400

    added ranking page to students

--- src/components/Student/StudentRanking.jsx
+++ src/components/Student/StudentRanking.jsx
@@ -12,83 +12,105 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { useStyles } from "../../constants";
 
 function StudentRanking() {
-  //TODO: Change API endpoint and related
-  const classes = useStyles();
+	//TODO: Change API endpoint and related
+	const classes = useStyles();
 
-  const [students, setStudents] = useState([]);
+	const [students, setStudents] = useState([]);
 
-  const studentsEndpoint = "https://www.mecallapi.com/api/users?sort_column=id&sort_order=desc&page=1&per_page=10";
+	const getStudents = async () => {
+		const response = await fetch(
+			process.env.REACT_APP_API_URL + "ranking",
+			{
+				method: "GET",
+				headers: new Headers({
+					Authorization:
+						"Bearer " + localStorage.getItem("accessToken"),
+				}),
+			}
+		).then((response) => response.json());
+		setStudents(response);
+	};
 
-  const getStudents = async () => {
-    const response = await fetch(studentsEndpoint, { method: "GET" }).then(
-      (response) => response.json()
-    );
-    setStudents(response['data']);
-  };
+	useEffect(() => {
+		getStudents();
+	}, []);
 
-  useEffect(() => {
-    getStudents();
-  }, []);
+	function createData(id, name, surname, degree, gpa) {
+		return { id, name, surname, degree, gpa };
+	}
 
-  function createData(id, name, surname, degree, gpa) {
-    return { id, name, surname, degree, gpa };
-  }
+	console.log(students);
 
-  var rows = students.map(function (student) {
-    return createData(
-      student.id,
-      student.fname,
-      student.lname,
-      student.fname + student.lname,
-      student.id
-    );
-  });
+	var rows = students.map(function (student) {
+		return createData(
+			student.id,
+			student.user.names,
+			student.user.surnames,
+			student.programCode + student.programYear,
+			student.gpa
+		);
+	});
 
-  return (
-    <Grid container justifyContent="center">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5" className="pb-7">
-          Top 10 estudiantes con mejor GPA
-        </Typography>
-        <Paper sx={{ width: "100%", overflow: "hidden" }}>
-          <TableContainer component={Paper}>
-            <Table
-              stickyHeader
-              sx={{ minWidth: 650 }}
-              aria-label="sticky table"
-            >
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center">ID</TableCell>
-                  <TableCell align="left">Nombre</TableCell>
-                  <TableCell align="left">Apellido</TableCell>
-                  <TableCell align="left">Carrera</TableCell>
-                  <TableCell align="center">GPA</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell align="center" component="th" scope="row">
-                      {row.id}
-                    </TableCell>
-                    <TableCell align="left">{row.name}</TableCell>
-                    <TableCell align="left">{row.surname}</TableCell>
-                    <TableCell align="left">{row.degree}</TableCell>
-                    <TableCell align="center">{row.gpa}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Paper>
-      </div>
-    </Grid>
-  );
+	return (
+		<Grid container justifyContent='center'>
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Typography component='h1' variant='h5' className='pb-7'>
+					Top 10 Índices de la Aplicación
+				</Typography>
+				<Paper sx={{ width: "100%", overflow: "hidden" }}>
+					<TableContainer component={Paper}>
+						<Table
+							stickyHeader
+							sx={{ minWidth: 650 }}
+							aria-label='sticky table'
+						>
+							<TableHead>
+								<TableRow>
+									<TableCell align='center'>ID</TableCell>
+									<TableCell align='left'>Nombre</TableCell>
+									<TableCell align='left'>Apellido</TableCell>
+									<TableCell align='left'>Carrera</TableCell>
+									<TableCell align='center'>GPA</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{rows.map((row) => (
+									<TableRow
+										key={row.id}
+										sx={{
+											"&:last-child td, &:last-child th":
+												{ border: 0 },
+										}}
+									>
+										<TableCell
+											align='center'
+											component='th'
+											scope='row'
+										>
+											{row.id}
+										</TableCell>
+										<TableCell align='left'>
+											{row.name}
+										</TableCell>
+										<TableCell align='left'>
+											{row.surname}
+										</TableCell>
+										<TableCell align='left'>
+											{row.degree}
+										</TableCell>
+										<TableCell align='center'>
+											{row.gpa}
+										</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</Paper>
+			</div>
+		</Grid>
+	);
 }
 
 export default StudentRanking;
